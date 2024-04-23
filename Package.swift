b2a251001052182da4789eb0c1d3bf43966abf7a
Prashant Bhujbal <prashant.bhujbal@bloomreach.com>
2024-04-23 13:15:28 +0530

    Added required PrivacyInfo file as required by Apple's App Store Policy
    https://developer.apple.com/documentation/bundleresources/privacy_manifest_files/describing_data_use_in_privacy_manifests

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "discovery-ios-sdk",
-            dependencies: []),
+            dependencies: [],
+            resources: [.copy("PrivacyInfo.xcprivacy")]),
         .testTarget(
             name: "discovery-ios-sdkTests",
             dependencies: ["discovery-ios-sdk"]),
